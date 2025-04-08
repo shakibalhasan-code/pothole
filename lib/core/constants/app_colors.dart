4b29c5390980c949631270541b4310cf29a1d529
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-08 08:25:53 +0600

    Add AppColors class with color constants for the application

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xff4A90E2);
+  static Color primaryDarkColor = const Color(0xff096DE2);
+
+  static Color whiteColor = const Color(0xffF9F9F9);
+  static Color pimaryLightColor = const Color(0xffE4EDF7);
+  static Color lightGrayColor = const Color(0xffE0E0E0);
+
+  static Color dangerColor = const Color(0xffF53838);
+
+  static Color darkColor = const Color(0xff0C0C0C);
+
+  static Color grayColor = const Color(0xff707070);
+  static Color darkGrayColor = const Color(0xff212121);
+}
