28375418307d8b7664329789b01038eff87946c9
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-08 10:37:15 +0600

    Add CustomButton widget for reusable button component

---
+++ lib/core/components/custom_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jourapothole/core/constants/app_colors.dart';
+import 'package:jourapothole/core/constants/app_text_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.buttonTitle,
+    required this.onTap,
+    this.isRounded = true,
+    this.isFilled = true,
+    this.isRed = false,
+    this.widthFactor = 1,
+  });
+
+  final VoidCallback onTap;
+  final String buttonTitle;
+  final bool isRounded;
+  final bool isFilled;
+  final bool isRed;
+  final double widthFactor;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: FractionallySizedBox(
+        widthFactor: widthFactor,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 12.h),
+          decoration: BoxDecoration(
+            color: (isFilled && !isRed) ? AppColors.primaryColor : null,
+            border: Border.all(
+              color: isRed ? AppColors.dangerColor : AppColors.primaryColor,
+            ),
+            borderRadius: BorderRadius.circular(isRounded ? 100 : 12),
+          ),
+          child: Center(
+            child: Text(
+              buttonTitle,
+              style: AppTextStyles.bodyLarge.copyWith(
+                color:
+                    isFilled
+                        ? AppColors.whiteColor
+                        : isRed
+                        ? AppColors.dangerColor
+                        : AppColors.primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
