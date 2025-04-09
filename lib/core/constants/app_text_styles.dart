271e72747623a91e55171e59c5e193e2d5b5d412
taskinnasif144 <taskinnasif144@gmail.com>
2025-04-09 10:53:25 +0600

    Update text styles for consistency and add flutter_svg dependency

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -4,12 +4,22 @@ import 'package:jourapothole/core/constants/app_colors.dart';
 
 class AppTextStyles {
   static TextStyle headerLarge = TextStyle(
-    fontSize: 40.sp,
+    fontSize: 48.sp,
     fontWeight: FontWeight.w700,
     color: AppColors.darkColor,
   );
   static TextStyle bodyLarge = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkColor,
+  );
+  static TextStyle bodyMedium = TextStyle(
     fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkColor,
+  );
+  static TextStyle bodySmall = TextStyle(
+    fontSize: 14.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.darkColor,
   );
